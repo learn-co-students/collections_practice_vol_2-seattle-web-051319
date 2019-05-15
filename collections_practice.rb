77e16303b08ddc8cd294ed23e1343ff9856eb805
AustinBH <harlowaustin2@gmail.com>
2019-05-15 09:16:31 -0700

    Completed several tests

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
 # your code goes here
+require 'pry'
+
+def begins_with_r(array)
+  array.all? {|word| word.start_with?("r")}
+end
+
+def contain_a(array)
+  new_array = []
+  array.collect do |word|
+    #binding.pry
+    if word.include?("a")
+    new_array << word
+    end
+  end
+  new_array
+end
+
+def first_wa(array)
+  array.find {|word| word.to_s.start_with?("wa")}
+end
+
+def remove_non_strings(array)
+  array.keep_if {|element| element.to_s == element}
+end
