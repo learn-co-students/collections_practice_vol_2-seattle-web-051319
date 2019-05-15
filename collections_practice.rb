401ba7abbe53e707d3240436e242693dea3bdd44
AustinBH <harlowaustin2@gmail.com>
2019-05-15 10:02:10 -0700

    A few small updates

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,3 +23,28 @@ end
 def remove_non_strings(array)
   array.keep_if {|element| element.to_s == element}
 end
+
+def count_elements(array)
+  new_array = array.each_with_index do |element, index|
+    binding.pry
+    if element == array[index+1]
+      array.delete[index]
+      element[:count] = 2
+    else
+      element[:count] = 1
+    end
+  end
+  new_array
+end
+
+def merge_data(array_one, array_two)
+
+end
+
+def find_cool(array)
+
+end
+
+def organize_schools(hash)
+
+end
