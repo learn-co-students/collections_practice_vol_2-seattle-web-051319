10495723409cb61d17e3c7ea96da63a6e53bf4ad
AustinBH <harlowaustin2@gmail.com>
2019-05-15 13:52:44 -0700

    All tests passing

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,26 +25,57 @@ def remove_non_strings(array)
 end
 
 def count_elements(array)
-  new_array = array.each_with_index do |element, index|
-    binding.pry
-    if element == array[index+1]
-      array.delete[index]
-      element[:count] = 2
-    else
+  array.each_with_index do |element, index|
+    element[:count] = 1
+    if element[:name] == array[index-1][:name]
+      array.delete_at(index)
+      array[index-1][:count] += 1
+    end
+  end
+  array.each do |element|
+    if element[:count] == nil
       element[:count] = 1
+    end
+  end
+end
+
+
+def merge_data(array_one, array_two)
+  new_array = []
+  new_hash = {}
+  new_hash_two = {}
+  array_two_keys = array_two[0].keys
+    array_one.each do |info|
+      merging_hashes = info.merge(array_two[0][info[:first_name]])
+      if info[:first_name] == array_two_keys.find {|name| name}
+        new_hash = merging_hashes
+      else
+        new_hash_two = merging_hashes
+      end
+    end
+  new_array << new_hash
+  new_array << new_hash_two
+  new_array
+end
+
+def find_cool(array)
+  new_array = []
+  array.each do |element|
+    if element[:temperature] == "cool"
+      new_array << element
     end
   end
   new_array
 end
 
-def merge_data(array_one, array_two)
-
-end
-
-def find_cool(array)
-
-end
-
 def organize_schools(hash)
-
+  new_hash = {}
+  hash.each do |class_name, location|
+    if new_hash[location.values.join(" ")] == nil
+      new_hash[location.values.join(" ")] = [class_name]
+    else
+      new_hash[location.values.join(" ")] << class_name
+    end
+  end
+  new_hash
 end
